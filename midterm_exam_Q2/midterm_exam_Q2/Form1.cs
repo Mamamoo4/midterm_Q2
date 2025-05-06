bc83df7bebe40e319b8b1a77aa00585c119b758c
Mamamoo4 <elmer0219.hsu@gmail.com>
2025-05-06 10:32:23 +0800

    Add files via upload

---
+++ midterm_exam_Q2/midterm_exam_Q2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace midterm_exam_Q2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i >= 55 && i <= 67 && (i % 2 != 0))
+                {                  
+                    label1.Text +=  i.ToString() + " , " ;
+                    sum += i;                    
+                }
+            }
+            label1.Text +=  $"\n\n他們之間的和為{sum}";
+        }
+    }
+}
